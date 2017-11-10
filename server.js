3a93d9669bce55c5e845e1e5a39e6fcf3f51706b
Chris Houdlette <choudlette@gmail.com>
2017-11-09 19:32:18 -0700

    vue is not compiling

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const indexHTML = (()=> {
+  return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8")
+})();
+
+const port = process.env.PORT || 3000;
+
+app.use("/dist", express.static(path.resolve(__dirname, "./dist")))
+
+app.get('*', (req,res)=> {
+  res.send(indexHTML);
+  res.end();
+})
+
+app.listen(port, ()=> {
+  console.log(`server running at http://localhost:${port}`);
+});
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+
+const app = new Vue({
+hello: "hi there"
+});
+
+export {app};
