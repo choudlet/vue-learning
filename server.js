bcfbe3d120131ddc708247560ac1d129aa5446df
Chris Houdlette <choudlette@gmail.com>
2017-11-10 13:05:40 -0600

    still working on hot reloading ish

--- server.js
+++ server.js
@@ -7,11 +7,13 @@ const indexHTML = (()=> {
   return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8")
 })();
 
+require('./build/dev-server')(app);
+
+
 const port = process.env.PORT || 3000;
 
 app.use("/dist", express.static(path.resolve(__dirname, "./dist")))
 
-require('./build/dev-server')(app);
 
 app.get('*', (req,res)=> {
   res.send(indexHTML);
